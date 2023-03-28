22bdeb3d214a6226486119ff1caeb67770bb96ef
Kevin Cho <kevcho604@gmail.com>
2023-03-28 15:55:02 -0700

    Adds responsive counter to characters entered into tweet text area using jquery event handler. Adds this via new script composter-char-counter.js

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,23 @@
+// Character counter function
+
+$(document).ready(function() {
+  // Display default 140 char count
+  $('#char-counter').text('140');
+  // event handler to textarea element for form inside of new-tweet
+  $('#tweet-text').keyup(function () {
+    let chars = 140 - $(this).val().length; // jquery val() method returns the current value of the textarea element
+    let negativeChars = -1; 
+    if (chars < 0) {
+      // if < 0, display red negative numbers using css 
+      $('#char-counter').css('color', 'red'); 
+    } else {
+      // reset color back to default 
+      $('#char-counter').css('color', '');
+    }
+    // Update the text of the element id char-counter to display chars left 
+    // $('#char-counter').text(chars); < bad practice
+    // traverse up the DOM tree with .closest from textarea until it finds the nearest form element
+    // .find goes back down the DOM tree to find the id char-counter  
+    $(this).closest('form').find('#char-counter').text(chars);
+  }); 
+});
