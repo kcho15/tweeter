c6b24b8e234f3c7155b3b73275b9678cb46aac3e
Kevin Cho <kevcho604@gmail.com>
2023-03-29 20:30:36 -0700

    Adds refetch of tweets on submission by preventing browser default refresh behavior

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,44 +4,8 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
 $(document).ready(() => {
-
-  // const data = [
-  //   {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": "/images/avatar1.PNG"
-  //       ,
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     "created_at": 1461116232227
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Descartes",
-  //       "avatars": "/images/avatar2.PNG",
-  //       "handle": "@rd" },
-  //     "content": {
-  //       "text": "Je pense , donc je suis"
-  //     },
-  //     "created_at": 1461113959088
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Nyamero",
-  //       "avatars": "/images/avatar5.PNG",
-  //       "handle": "@docmilo" },
-  //     "content": {
-  //       "text": "nyan"
-  //     },
-  //     "created_at": 1461113959069
-  //   }
-
-  // ]
-  // grab the tweet-container section in the DOM 
   const $tweetContainer = $("#tweets-container");
 
   const renderTweets = function(tweets) {
@@ -80,64 +44,52 @@ $(document).ready(() => {
     return $tweet;
   };
 
-  // Jquery function that loads tweets 
-  
+  // grab the form from the DOM
+  const $form = $('#new-tweet-form');
 
-
-  // renderTweets(data); for testing 
-
-
-// grab the form from the DOM
-const $form = $('#new-tweet-form');
-
-// add a submit event handler to the form
-$form.on('submit', (event) => {
-  // grab text from submission 
-  const $tweetText = $('#tweet-text');
-  
-  // Max character limit
-  const maxCharLimit = 140; 
-
-  // Validation checks 
-  // Error if no text is submitted 
-  if ($tweetText.val().trim().length === 0) { 
-    alert('Error: Tweet content is empty.'); 
+  // add a submit event handler to the form
+  $form.on('submit', (event) => {
+  // prevent refresh, tweets load on page after submission
     event.preventDefault();
-    return; 
-  } 
-  // Error if tweet is above max char limit 
-  if ($tweetText.val().length > maxCharLimit) {
-    alert('Error: Tweet length exceeds maximum limit.');
-    event.preventDefault();
-    return;
-  }
-
+    
+    // grab text from submission
+    const $tweetText = $('#tweet-text');
   
-  event.preventDefault(); // stop the browser from default behavior of refreshing the page 
-  console.log('New tweet incoming!');
+    // Max character limit
+    const maxCharLimit = 140;
 
-  const urlencoded = $form.serialize(); // gives us back urlencoded data 
-  console.log(urlencoded); // debugging
+    // Validation checks
+    // Error if no text is submitted
+    if ($tweetText.val().trim().length === 0) {
+      alert('Error: Tweet content is empty.');
+      return;
+    }
+    // Error if tweet is above max char limit
+    if ($tweetText.val().length > maxCharLimit) {
+      alert('Error: Tweet length exceeds maximum limit.');
+      return;
+    }
+  
+    console.log('New tweet incoming!'); // debugging
 
-  $.ajax({
-    method: 'POST',
-    url: '/tweets',
-    data: urlencoded,
-  }).then((newTweet) => {
-    // .catch (err)
-    console.log(newTweet);
+    const urlencoded = $form.serialize(); // gives us back urlencoded data
+  
+    console.log(urlencoded); // debugging
 
-    });
+    $.ajax({
+      method: 'POST',
+      url: '/tweets',
+      data: urlencoded,
+    }).then(loadTweets);
   });
 
   const loadTweets = () => {
     $.ajax({
       method: 'GET',
       url: '/tweets',
-      success: renderTweets
-    });
+    }).then(renderTweets);
   };
-
-  loadTweets(); // testing 
-
-}); 
+  
+  loadTweets(); // testing
+  
+});
